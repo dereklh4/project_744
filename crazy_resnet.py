eaac989fb3cf6e41dbc14605e0f663d666190b02
Saurabh <saurabh@madstreetden.com>
2018-12-03 23:38:30 -0600

    Another bug fix in import statement

--- crazy_resnet.py
+++ crazy_resnet.py
@@ -2,6 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import numpy as np
 import time
 from collections import defaultdict
 metrics_dict = defaultdict(list)
