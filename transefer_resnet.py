64a41221c46b3da6daf6c49a3bc09de0782e84f4
Saurabh <saurabh@madstreetden.com>
2018-12-08 10:21:53 -0600

    Bug fix in transfer learning code

--- transefer_resnet.py
+++ transefer_resnet.py
@@ -258,6 +258,7 @@ def main():
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
     transform_test = transforms.Compose([
+    transforms.Resize(224),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
